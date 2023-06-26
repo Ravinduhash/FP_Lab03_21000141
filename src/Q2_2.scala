ace5b3e149839e90ce1ae76f8742bc65bec3011f
Ravindu Hasanka <rhatu2000@gmail.com>
2023-06-27 00:31:30 +0530

    finalize

--- src/Q2_2.scala
+++ src/Q2_2.scala
@@ -1,16 +1,29 @@
-import scala.io.StdIn._
-object Q2_2 extends App{
-  def fun(arr:List[String])=arr.filter(_.length>5)
+//import scala.io.StdIn._
+//object Q2 extends App{
 //  var newlist = new Array[String](0)
-
+//  def func(str:String):Array[String]={
+//    if(str.length>5){
+//      Array(str)
+//    }
+//    else {
+//      Array()
+//    }
+//  }
+//
+//  def func1(str:Array[String]):Array[String]={
+//    if(str.length>1){
+//      var newarr: Array[String] = Array.concat(func(str.head), func1(str.tail))
+//    }
+//    else {
+//      func(str.head)
+//    }
+//  }
 //  var n = readLine("Please enter no of strings: ").toInt
-//  var arr : List[String]
+//  var arr = new List[String](n)
 //  var a=0
 //  for(a<-0 to n-1){
-//    arr = arr :: readLine("Enter string: ")
+//    arr(a)= readLine("Enter string: ")
 //  }
-
-  println(fun(List("My","name","is","ravindu","hasanka","Athukorala","hi","all")))
-
-
-}
+//
+//
+//}
